61fe580723a205386a7701e5b9d00f2213c66321
TZe-0xff <sa.tybos@gmail.com>
2023-07-10 17:45:47 +0200

    Added BSD clause 3 license

--- thumb2ISS/instructions/ldmdb.py
+++ thumb2ISS/instructions/ldmdb.py
@@ -1,3 +1,11 @@
+#
+# Copyright (c) 2023 Thibaut Zeissloff.
+#
+# This file is part of Thumb2ISS
+# (see https://github.com/TZe-0xff/thumb2ISS).
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+#
 import re, logging
 
 log = logging.getLogger('Mnem.LDMDB')
--- thumb2ISS/test/_testing.py
+++ thumb2ISS/test/_testing.py
@@ -1,3 +1,11 @@
+#
+# Copyright (c) 2023 Thibaut Zeissloff.
+#
+# This file is part of Thumb2ISS
+# (see https://github.com/TZe-0xff/thumb2ISS).
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+#
 import os,sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from thumb2ISS.core import Core, Register
